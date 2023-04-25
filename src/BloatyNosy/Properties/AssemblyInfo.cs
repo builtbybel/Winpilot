b1ba52bdd9e7736826f62a72f345eb097fe281a4
Belim <belim@builtbybel.com>
2023-04-25 18:18:46 +0200

    Update AssemblyInfo.cs

--- src/BloatyNosy/Properties/AssemblyInfo.cs
+++ src/BloatyNosy/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.50.1")]
-[assembly: AssemblyFileVersion("0.50.1")]
+[assembly: AssemblyVersion("0.50.2")]
+[assembly: AssemblyFileVersion("0.50.2")]
