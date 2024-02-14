bda02b1d3a6de8b3a9c42c5ce3cbb9ee7c74e41a
Belim <github@builtbybel.com>
2024-02-14 09:00:24 +0100

    Clean-up classic Bloatynosy ballast

--- mods/update_store-apps.ps1
+++
@@ -1,4 +0,0 @@
-$namespaceName = "root\cimv2\mdm\dmmap"
-$className = "MDM_EnterpriseModernAppManagement_AppManagement01"
-$wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
-$result = $wmiObj.UpdateScanMethod()
