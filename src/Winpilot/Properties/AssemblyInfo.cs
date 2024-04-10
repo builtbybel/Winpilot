0310cf8efbcf372f8e33ec5e4219fee375e683de
Belim <belim@builtbybel.com>
2024-04-10 08:58:01 +0200

    Update AssemblyInfo.cs

--- src/Winpilot/Properties/AssemblyInfo.cs
+++ src/Winpilot/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.40")]
-[assembly: AssemblyFileVersion("3.3.40")]
+[assembly: AssemblyVersion("3.4.0")]
+[assembly: AssemblyFileVersion("3.4.0")]
